a93643f565eabc3f082b35fb861f587e4f7fd844
Marco Calderon <marcocald1998@gmail.com>
2021-02-27 17:15:26 -0500

    lab6

---
+++ lab6/index.js
@@ -0,0 +1,23 @@
+import Express from 'express';
+
+const App = Express();
+const port = 45030;
+
+const person = {
+    name: "Marco Calderon",
+    favoritecolor: "purple"
+}
+
+App.get("/", function(req,res){
+    res.send("Hello World!");
+});
+
+App.get("/person", function(req, res){
+    res.json(person);
+});
+
+
+
+App.listen(port, function() {
+    console.log("Server is running");
+});
